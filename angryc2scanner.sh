756b0173897bc67e911c8b979e46689319b21164
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2026-03-04 02:48:43 -0500

    Add a friendly c2 scanner

--- angryc2scanner.sh
+++ angryc2scanner.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# Like c2scanner but ANGRY
 
 mkdir -p scans
 
@@ -17,6 +18,13 @@ do
                 echo "====== NEW STUFF HAPPENING ======"
                 echo "event at $(date)"
                 diff "scans/$FNAME" "scans/$FNAME_LAST"
+
+                echo "====== COMMITING MURDER ======"
+                ss -tpn | grep -Po 'pid=\K[0-9]+' > hitlist
+                while IFS= read -r line; do
+                   sudo kill -9 "$line"
+                   echo "done murding $line"
+                done < hitlist
         fi
 
         FNAME_LAST=$FNAME
