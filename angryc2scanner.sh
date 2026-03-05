e0e90d05680f0e3c9019344a7271cf86aab454e5
0xL30N3 <dudepro1111@gmail.com>
2026-03-05 00:54:36 -0500

    added parent killer

--- angryc2scanner.sh
+++ angryc2scanner.sh
@@ -9,25 +9,32 @@ FNAME_LAST="$(date +%s)ProMax"
 
 while true
 do
-        FNAME="$(date +%s)"
-        ss -tupn > "scans/$FNAME"
+  FNAME="$(date +%s)"
+  ss --no-header -tupn > "scans/$FNAME"
 
-        DIFF=$(diff "scans/$FNAME" "scans/$FNAME_LAST")
-        if [ "$DIFF" != "" ];
-        then
-                echo "====== NEW STUFF HAPPENING ======"
-                echo "event at $(date)"
-                diff "scans/$FNAME" "scans/$FNAME_LAST"
+  DIFF=$(diff "scans/$FNAME" "scans/$FNAME_LAST")
+  if [ "$DIFF" != "" ];
+  then
+    echo "====== NEW STUFF HAPPENING ======"
+    echo "event at $(date)"
+    diff "scans/$FNAME" "scans/$FNAME_LAST"
 
-                echo "====== COMMITING MURDER ======"
-                ss -tpn | grep -Po 'pid=\K[0-9]+' > hitlist
-                while IFS= read -r line; do
-                   sudo kill -9 "$line"
-                   echo "done murding $line"
-                done < hitlist
-        fi
+    echo "====== COMMITING MURDER ======"
+    ss -tpn | grep -Po 'pid=\K[0-9]+' > hitlist
+    while IFS= read -r line; do
+       ppid=$(ps --no-headers -fp "$line" | awk '$1 { print $3 }')
+       sudo kill -9 "$line"
+       while [ "$ppid" != "$line" ] && [ "$ppid" != "1" ]; do
+         prev=$ppid
+         ppid=$(ps --no-headers -fp "$prev" | awk '$1 { print $3 }')
+         kill -9 prev;
+         echo "done murding $prev"
+         done
+       echo "done murding $line"
+    done < hitlist
+  fi
 
-        FNAME_LAST=$FNAME
+  FNAME_LAST=$FNAME
 
-        sleep .5
+  sleep .5
 done
