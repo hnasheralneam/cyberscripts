fc6d84473c67579146745234890fe267b9d54151
0xL30N3 <dudepro1111@gmail.com>
2026-03-16 01:48:51 -0400

    major patch incoming

--- testing/revertProxmox.sh
+++ testing/revertProxmox.sh
@@ -2,16 +2,22 @@
 source ./.env
 URL=https://10.67.3.2:8006/
 printf "Reverting Ubuntu\n"
-curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/121/snapshot/KnownGood/rollback 
+curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/121/snapshot/base/rollback 
 printf "\nReverting EndeavorOS\n"
-curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/122/snapshot/baseos/rollback 
+curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/122/snapshot/base/rollback 
+printf "\nReverting Fedora\n"
+curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/139/snapshot/base/rollback 
+printf "\nReverting Oracle\n"
+curl -XPOST --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/140/snapshot/base/rollback 
 printf "\n"
 
 sleep 5;
 
 status1=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/121/status/current | jq .data.status)
 status2=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/122/status/current | jq .data.status) 
-while [ "$status1" != '"running"' ] || [ "$status2" != '"running"' ]; do
+status3=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/139/status/current | jq .data.status)
+status4=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/140/status/current | jq .data.status)
+while [ "$status1" != '"running"' ] || [ "$status2" != '"running"' ] || [ "$status3" != '"running"' ] || [ "$status4" != '"running"' ]; do
   if [ "$status1" != '"running"' ];then
     status1=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/121/status/current | jq .data.status)
     printf "Vm1: $status1\n"
@@ -20,10 +26,18 @@ while [ "$status1" != '"running"' ] || [ "$status2" != '"running"' ]; do
     status2=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/122/status/current | jq .data.status) 
     printf "Vm2: $status2\n"
   fi
-  if [ "$status1" = "running" ] && [ "$status2" = "running" ];then
+  if [ "$status3" != '"running"' ];then
+    status3=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/139/status/current | jq .data.status) 
+    printf "Vm3: $status3\n"
+  fi
+  if [ "$status4" != '"running"' ];then
+    status4=$(curl --silent --insecure -H "Authorization: $api_token" "$URL"api2/json/nodes/vm1/qemu/140/status/current | jq .data.status) 
+    printf "Vm4: $status4\n"
+  fi
+  if [ "$status1" = "running" ] && [ "$status2" = "running" ] && [ "$status3" = "running" ] && [ "$status4" = "running" ];then
     printf "[DONE]\n"
     exit 1;
   fi
   sleep 1;
 done
-printf "Both vms completed reverting\n"
+printf "All vms completed reverting\n"
